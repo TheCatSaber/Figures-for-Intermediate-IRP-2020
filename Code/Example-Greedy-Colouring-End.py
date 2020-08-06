680a1a342009911f9723d92f14887375eeadb440
TheCatSaber <66741113+TheCatSaber@users.noreply.github.com>
2020-08-06 10:05:44 +0100

    Added Greedy Colouring Example Figures

---
+++ Code/Example-Greedy-Colouring-End.py
@@ -0,0 +1,45 @@
+#Imports
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+#Constants
+OUTPUT_COLOURS = ["blue", "red", "green"]
+
+
+#Functions
+def numerical_to_colours(colouring):
+    output_colouring = []
+    for vertex, colour in colouring.items():
+        try:
+            output_colouring.append(OUTPUT_COLOURS[colour])
+        except IndexError:
+            raise IndexError
+    return output_colouring
+
+
+#Define the graph
+G = nx.Graph()
+G.add_nodes_from([chr(i) for i in range(65, 69)])
+edges = [('A', 'B'), ('A', 'C'), ('B', 'C'), ('B', 'D')]
+G.add_edges_from(edges)
+
+#Define the colouring
+colouring = {'A': 1, 'B': 0, 'C': 2, 'D': 1}
+#Convert to colours
+output_colouring = numerical_to_colours(colouring)
+
+#Define the positions of the vertices
+
+vertices = list(G.nodes())
+
+pos = {'A': (-1, 0), 'B': (0, -0.5), 'C': (1, 0), 'D': (0, -1)}
+
+#Labels of node name
+
+labels = {vertex: vertex for vertex in G.nodes()}
+
+#Draw the graph
+nx.draw_networkx(G, pos=pos, with_labels=True, node_color=output_colouring, linewidths=1.75, edgecolors="black", width=2, labels=labels, font_color="white", font_weight="heavy")
+plt.savefig("Example-Greedy-Colouring-End.png")
+plt.show()
---
+++ Code/Example-Greedy-Colouring-Start.py
@@ -0,0 +1,25 @@
+#Imports
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+#Define the graph
+G = nx.Graph()
+G.add_nodes_from([chr(i) for i in range(65, 69)])
+edges = [('A', 'B'), ('A', 'C'), ('B', 'C'), ('B', 'D')]
+G.add_edges_from(edges)
+
+#Define the positions of the vertices
+
+vertices = list(G.nodes())
+
+pos = {'A': (-1, 0), 'B': (0, -0.5), 'C': (1, 0), 'D': (0, -1)}
+
+#Labels of node name
+
+labels = {vertex: vertex for vertex in G.nodes()}
+
+#Draw the graph
+nx.draw_networkx(G, pos=pos, with_labels=True, node_color="white", linewidths=1.75, edgecolors="black", width=2, labels=labels, font_color="black", font_weight="medium")
+plt.savefig("Example-Greedy-Colouring-Start.png")
+plt.show()
