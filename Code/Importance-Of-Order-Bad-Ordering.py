96093de627c55b425fcc865f2731c591607a2901
TheCatSaber <66741113+TheCatSaber@users.noreply.github.com>
2020-08-06 08:42:57 +0100

    Importance of Ordering for Greedy Colouring Code
    Demonstrates differences between a good and a bad ordering

---
+++ Code/Importance-Of-Order-Bad-Ordering.py
@@ -0,0 +1,47 @@
+#Imports
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#Constants
+OUTPUT_COLOURS = ["blue", "red", "green", "#ff0073"]
+
+
+#Functions
+def numerical_to_colours(colouring):
+    output_colouring = []
+    for vertex, colour in colouring.items():
+        try:
+            output_colouring.append(OUTPUT_COLOURS[colour])
+        except IndexError:
+            raise IndexError
+    return output_colouring
+
+
+#Define the graph
+G = nx.Graph()
+G.add_nodes_from([chr(i) for i in range(65, 70)])
+edges = [('A', 'B'), ('A', 'C'), ('A', 'D'), ('A', 'E'), ('B', 'C'), ('C', 'D'), ('D', 'E')]
+G.add_edges_from(edges)
+
+#Define the colouring
+colouring = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 1}
+#Convert to colours
+output_colouring = numerical_to_colours(colouring)
+
+#Define the positions of the vertices
+
+vertices = list(G.nodes())
+
+pos = {'A': (0, 0), 'B': (-1, 0), 'C': (-np.sqrt(2)/2, -np.sqrt(2)/2), 'D': (np.sqrt(2)/2, -np.sqrt(2)/2), 'E': (1, 0)}
+
+#Labels to represent colouring order
+
+labels = {'A': 1, 'B': 2, 'C': 4, 'D': 5, 'E': 3}
+
+#Draw the graph
+nx.draw_networkx(G, pos=pos, with_labels=True, node_color=output_colouring, linewidths=1.75, edgecolors="black", width=2, labels=labels, font_color="white", font_weight="heavy")
+
+plt.savefig("Importance-Of-Order-Bad-Ordering.png")
+plt.show()
---
+++ Code/Importance-Of-Order-Good-Ordering.py
@@ -0,0 +1,46 @@
+#Imports
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#Constants
+OUTPUT_COLOURS = ["blue", "red", "green", "magenta"]
+
+
+#Functions
+def numerical_to_colours(colouring):
+    output_colouring = []
+    for vertex, colour in colouring.items():
+        try:
+            output_colouring.append(OUTPUT_COLOURS[colour])
+        except IndexError:
+            raise IndexError
+    return output_colouring
+
+
+#Define the graph
+G = nx.Graph()
+G.add_nodes_from([chr(i) for i in range(65, 70)])
+edges = [('A', 'B'), ('A', 'C'), ('A', 'D'), ('A', 'E'), ('B', 'C'), ('C', 'D'), ('D', 'E')]
+G.add_edges_from(edges)
+
+#Define the colouring
+colouring = {'A': 0, 'B': 1, 'C': 2, 'D': 1, 'E': 2}
+#Convert to colours
+output_colouring = numerical_to_colours(colouring)
+
+#Define the positions of the vertices
+
+vertices = list(G.nodes())
+
+pos = {'A': (0, 0), 'B': (-1, 0), 'C': (-np.sqrt(2)/2, -np.sqrt(2)/2), 'D': (np.sqrt(2)/2, -np.sqrt(2)/2), 'E': (1, 0)}
+
+#Labels to represent colouring order
+
+labels = {'A': 1, 'B': 4, 'C': 2, 'D': 3, 'E': 5}
+
+#Draw the graph
+nx.draw_networkx(G, pos=pos, with_labels=True, node_color=output_colouring, linewidths=1.75, edgecolors="black", width=2, labels=labels, font_color="white", font_weight="heavy")
+plt.savefig("Importance-Of-Order-Good-Ordering.png")
+plt.show()
